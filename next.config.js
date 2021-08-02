9accf2e133e92b3400e7ae5cbed26da0c24b0401
Saurish Srivastava <saurishs1215@gmail.com>
2021-08-02 15:03:15 -0400

    [add] armstrong paper

--- next.config.js
+++ next.config.js
@@ -6,6 +6,12 @@ module.exports = {
         destination: "https://donorbox.org/ataraxia-project-envision-us/",
         permanent: true,
       },
+      {
+        source: "/papers/armstrong",
+        destination:
+          "https://drive.google.com/file/d/1KE9h4wrsUUMVDqtY2SEg0APCWiq76Sl9/view?usp=sharing",
+        permanent: true,
+      },
     ];
   },
 };
