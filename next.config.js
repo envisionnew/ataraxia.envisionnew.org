abc435bab24f52511045018bedbe91739cfc2f16
Saurish Srivastava <saurishs1215@gmail.com>
2021-08-02 21:55:02 -0400

    [:zap:] add allen paper

--- next.config.js
+++ next.config.js
@@ -12,6 +12,12 @@ module.exports = {
           "https://drive.google.com/file/d/1KE9h4wrsUUMVDqtY2SEg0APCWiq76Sl9/view?usp=sharing",
         permanent: true,
       },
+      {
+        source: "/papers/allen",
+        destination:
+          "https://drive.google.com/file/d/11bXDzVEVUe9b3ql4DCwYR2Eh_xB893sK/view?usp=sharing",
+        permanent: true,
+      },
     ];
   },
 };
