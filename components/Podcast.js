75ee6b8573261e49443611522afbddd7b6eab753
Saurish Srivastava <saurishs1215@gmail.com>
2021-08-04 08:40:51 -0700

    [:zap:] episode 1!

--- components/Podcast.js
+++ components/Podcast.js
@@ -4,14 +4,17 @@ export default function Podcast() {
       <div className="max-w-xl px-4 py-12 mx-auto sm:px-6 lg:max-w-6xl lg:px-8">
         <h1 className="mb-8 text-2xl font-bold tracking-normal text-center text-gray-800 md:leading-tight md:tracking-normal dark:text-gray-200 md:text-4xl">
           Listen to the{" "}
-          <span className="text-transparent bg-clip-text bg-gradient-to-r dark:bg-gradient-to-r from-rose-500 via-fuchsia-500 to-indigo-500 dark:from-rose-400 dark:via-fuchsia-400 dark:to-indigo-400">
+          <a
+            href="https://open.spotify.com/embed/episode/5d4dmTZNdsOKcXEyv8kzgu"
+            className="text-transparent underline bg-clip-text bg-gradient-to-r dark:bg-gradient-to-r from-rose-500 via-fuchsia-500 to-indigo-500 dark:from-rose-400 dark:via-fuchsia-400 dark:to-indigo-400"
+          >
             Speaking from Empathy
-          </span>{" "}
+          </a>{" "}
           Podcast
         </h1>
         <div className="max-w-4xl mx-auto">
           <iframe
-            src="https://open.spotify.com/embed/episode/70SELWZqw2sH49TEuxG97c"
+            src="https://open.spotify.com/embed/episode/5d4dmTZNdsOKcXEyv8kzgu"
             width="100%"
             height="232"
             frameBorder="0"
