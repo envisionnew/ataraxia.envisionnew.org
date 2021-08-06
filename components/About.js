c5bc516557f6d6c3058ae4fa0c7567b0ede6dfeb
Saurish Srivastava <saurishs1215@gmail.com>
2021-08-06 09:29:08 -0700

    [:zap:] add advisor

--- components/About.js
+++ components/About.js
@@ -1,38 +1,53 @@
+const advisors = [
+  {
+    name: "Rushil Srivastava",
+    website: "https:rushilsrivastava.com",
+    image: "rushil.jpeg",
+    bio: "Co-Founder at Simplify; University of California, Berkeley",
+  },
+];
+
 const volunteers = [
   {
     name: "Shehbaz Singh Virk",
     position: "Co-founder and Executive Director",
     website: "https://www.linkedin.com/in/shehbazvirk",
+    bio: "Undergrad @ University of California, Berkeley",
     image: "shehbaz.jpeg",
   },
   {
     name: "Saurish Srivastava",
     position: "Co-Founder and Director of Operations",
     website: "https://saurish.com",
+    bio: "Junior @ Evergreen Valley High School",
     image: "saurish.jpg",
   },
   {
     name: "Shaheryar Asad",
     position: "Director of Research",
     website: "https://www.linkedin.com/in/shaheryar-asad-0089b7140",
+    bio: "Undergrad @ University of California, Berkeley",
     image: "shaheryar.jpeg",
   },
   {
     name: "Leah Altamirano",
     position: "Director of Outreach",
     website: "https://www.instagram.com/leahaltamirano/",
+    bio: "Junior @ Eagle Rock High School",
     image: "leah.jpeg",
   },
   {
     name: "Becky Montes",
     position: "Director of Public Relations",
     website: "https://www.instagram.com/_beckymontes/",
+    bio: "Junior @ Eagle Rock High School",
     image: "becky.jpeg",
   },
   {
     name: "Varun Bommaji",
     position: "Secretary",
     website: "https://www.instagram.com/varunbommaji/",
+    bio: "Junior @ Evergreen Valley High School",
     image: "varun.jpeg",
   },
 ];
@@ -122,8 +137,54 @@ export default function About() {
                           {volunteer.name}
                         </h2>
                       </a>
-                      <h3 className="mb-3 text-gray-500">
+                      <h3 className="mb-1 text-gray-700 dark:text-gray-400">
                         {volunteer.position}
+                      </h3>
+                      <h4 className="mb-3 text-gray-400 dark:text-gray-500">
+                        {volunteer.bio}
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="mb-10 space-y-6">
+          <h1 className="text-xl font-bold text-center text-rose-600 dark:text-rose-300 md:text-2xl">
+            Our Advisors
+          </h1>
+          <div className="container px-5 py-8 mx-auto">
+            <div className="flex flex-wrap -m-4">
+              {advisors.map((advisor) => (
+                <div className="p-4 lg:w-1/4 md:w-1/2" key={advisor}>
+                  <div className="flex flex-col items-center h-full text-center">
+                    <img
+                      alt={`Image of ${advisor.name}`}
+                      className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-80 md:h-60"
+                      src={`/images/${advisor.image}`}
+                    />
+                    <div className="w-full">
+                      <a
+                        className={
+                          advisor.website
+                            ? "underline text-black dark:text-white hover:text-link-blue dark:hover:text-link-purple"
+                            : "cursor-text"
+                        }
+                        href={advisor.website ? advisor.website : null}
+                      >
+                        <h2
+                          className={
+                            advisor.website
+                              ? "text-lg font-medium text-black hover:text-link-blue dark:hover:text-link-purple title-font dark:text-gray-200"
+                              : "text-lg font-medium text-black title-font dark:text-gray-200"
+                          }
+                        >
+                          {advisor.name}
+                        </h2>
+                      </a>
+                      <h3 className="mb-1 text-gray-700 dark:text-gray-400">
+                        {advisor.bio}
                       </h3>
                     </div>
                   </div>
